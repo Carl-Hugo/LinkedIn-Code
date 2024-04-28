d039ed7f27e8d4d5ac8a2633c3d00e3fd4f831f2
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-04-28 16:01:27 -0400

    Add Singleton 1/2

---
+++ 2024-Q2/Patterns/Singleton/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class MySingleton
+{
+    private static MySingleton? _instance;
+    private MySingleton() { }
+    public static MySingleton Create()
+    {
+        if (_instance == null)
+        {
+            _instance = new MySingleton();
+        }
+        return _instance;
+    }
+}
+
+public class MySingletonWithLock
+{
+    private static MySingletonWithLock? _instance;
+    private static readonly object _myLock = new();
+    private MySingletonWithLock() { }
+    public static MySingletonWithLock Create()
+    {
+        lock (_myLock)
+        {
+            if (_instance == null)
+            {
+                _instance = new MySingletonWithLock();
+            }
+        }
+        return _instance;
+    }
+}
+
+public class MySimpleSingleton
+{
+    public static MySimpleSingleton Instance { get; } = new MySimpleSingleton();
+    private MySimpleSingleton() { }
+}
