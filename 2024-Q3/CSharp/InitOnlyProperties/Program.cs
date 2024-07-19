76cfb9e7f7a2a8953cd1be830a311875699a18b2
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-07-19 15:12:40 -0400

    Add init only prop

---
+++ 2024-Q3/CSharp/InitOnlyProperties/Program.cs
@@ -0,0 +1,8 @@
+﻿var person = new Person { Name = "Alice", Age = 30 };
+Console.WriteLine($"Hello, {person.Name}, you are {person.Age} years old!");
+
+public class Person
+{
+    public string Name { get; init; }
+    public int Age { get; init; }
+}
