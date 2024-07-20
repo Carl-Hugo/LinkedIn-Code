a92fc349ef49c48435fd74e0262b027b182051a9
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-07-20 11:14:14 -0400

    Add FileService class and AddExample method

--- 2024-Q3/PoornaStrategyReply/Program.cs
+++ 2024-Q3/PoornaStrategyReply/Program.cs
@@ -1,13 +1,15 @@
 #define V1
-#define V2
-#define V3
-#define V4
+#undef V2
+#undef V3
+#undef V4
+
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddExample();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", Ok<List<Flight>> (string fileExtension, IFileService fileService) => TypedResults.Ok(fileService.Import(fileExtension, Stream.Null)));
 app.Run();
 
 #if V4
@@ -54,6 +56,14 @@ public class CsvParser : BaseParser
         throw new NotImplementedException();
     }
 }
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddExample(this IServiceCollection services)
+    {
+        return services;
+    }
+}
 #elif V3
 public interface IFileParser
 {
@@ -61,7 +71,7 @@ public interface IFileParser
 }
 
 // Template Method + Chain of Responsibility
-public abstract class BaseParser(IFileParser? next) : IFileParser
+public abstract class BaseParser(IFileParser? next = default) : IFileParser
 {
     private readonly IFileParser? _next = next;
     protected abstract string FileExtension { get; }
@@ -86,25 +96,51 @@ public abstract class BaseParser(IFileParser? next) : IFileParser
 
 public class NoParserFoundException(string? fileExtension) : Exception($"No parser found for {fileExtension}.");
 
-public class JsonParser(IFileParser next) : BaseParser(next)
+public class JsonParser(IFileParser? next = default) : BaseParser(next)
 {
     protected override string FileExtension => ".json";
     protected override List<Flight> Parse(Stream fileStream)
     {
         // JSON parser code goes here
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        Console.WriteLine("JsonParser");
+        return [];
     }
 }
 
-public class CsvParser(IFileParser next) : BaseParser(next)
+public class CsvParser(IFileParser? next = default) : BaseParser(next)
 {
     protected override string FileExtension => ".csv";
     protected override List<Flight> Parse(Stream fileStream)
     {
         // CSV parser code goes here
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        Console.WriteLine("CsvParser");
+        return [];
     }
 }
+
+public class FileService(IFileParser fileParser) : IFileService
+{
+    private readonly IFileParser _fileParser = fileParser;
+    public List<Flight> Import(string fileExtension, Stream fileStream)
+    {
+        return _fileParser.Parse(fileExtension, fileStream);
+    }
+}
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddExample(this IServiceCollection services)
+    {
+        services
+            .AddSingleton<IFileParser>(new JsonParser(new CsvParser()))
+            .AddSingleton<IFileService, FileService>()
+        ;
+        return services;
+    }
+}
+
 #elif V2
 // Template Method
 public interface IFileParser
@@ -137,7 +173,9 @@ public class JsonParser : BaseParser
     protected override List<Flight> Parse(Stream fileStream)
     {
         // JSON parser code goes here
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        Console.WriteLine("JsonParser");
+        return [];
     }
 }
 
@@ -147,11 +185,41 @@ public class CsvParser : BaseParser
     protected override List<Flight> Parse(Stream fileStream)
     {
         // CSV parser code goes here
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        Console.WriteLine("CsvParser");
+        return [];
     }
 }
 
-public class CanNotParseException(string? fileExtension) : Exception($"Can't parse {fileExtension}.");
+public class FileService(IEnumerable<IFileParser> fileParsers) : IFileService
+{
+    private readonly IEnumerable<IFileParser> _fileParsers = fileParsers;
+
+    public List<Flight> Import(string fileExtension, Stream fileStream)
+    {
+        foreach (var parser in _fileParsers)
+        {
+            if (parser.CanParse(fileExtension))
+            {
+                return parser.Parse(fileExtension, fileStream);
+            }
+        }
+        throw new CanNotParseException(fileExtension);
+    }
+}
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddExample(this IServiceCollection services)
+    {
+        services
+            .AddSingleton<IFileParser, JsonParser>()
+            .AddSingleton<IFileParser, CsvParser>()
+            .AddSingleton<IFileService, FileService>()
+        ;
+        return services;
+    }
+}
 
 #elif V1
 // Template Method
@@ -187,7 +255,9 @@ public class JsonParser : BaseParser
     protected override List<Flight> Parse(Stream fileStream)
     {
         // JSON parser code goes here
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        Console.WriteLine("JsonParser");
+        return [];
     }
 }
 
@@ -197,9 +267,47 @@ public class CsvParser : BaseParser
     protected override List<Flight> Parse(Stream fileStream)
     {
         // CSV parser code goes here
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        Console.WriteLine("CsvParser");
+        return [];
+    }
+}
+
+public class FileService(IEnumerable<IFileParser> fileParsers) : IFileService
+{
+    private readonly IEnumerable<IFileParser> _fileParsers = fileParsers;
+
+    public List<Flight> Import(string fileExtension, Stream fileStream)
+    {
+        foreach (var parser in _fileParsers)
+        {
+            if (parser.TryParse(fileExtension, fileStream, out var flights))
+            {
+                return new(flights);
+            }
+        }
+        throw new CanNotParseException(fileExtension);
+    }
+}
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddExample(this IServiceCollection services)
+    {
+        services
+            .AddSingleton<IFileParser, JsonParser>()
+            .AddSingleton<IFileParser, CsvParser>()
+            .AddSingleton<IFileService, FileService>()
+        ;
+        return services;
     }
 }
 #endif
 
+public interface IFileService
+{
+    List<Flight> Import(string fileExtension, Stream fileStream);
+}
+
 public record class Flight();
+public class CanNotParseException(string? fileExtension) : Exception($"Can't parse {fileExtension}.");
