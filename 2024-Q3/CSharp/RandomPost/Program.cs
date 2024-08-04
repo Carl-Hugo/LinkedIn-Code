2ab93e128886893f9d86d231fa67d23873204baf
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-08-04 12:23:40 -0400

    Add Random

---
+++ 2024-Q3/CSharp/RandomPost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+// Using RandomNumberGenerator to shuffle and get a random item from a collection
+var rngItems = new[] { "apple", "banana", "cherry", "date" }.AsSpan();
+RandomNumberGenerator.Shuffle(rngItems);
+var rngItem = RandomNumberGenerator.GetItems<string>(rngItems, 1).Single();
+Console.WriteLine(rngItem);
+
+// Using Random.Shared for thread-safe random number generation
+var randomNumber = Random.Shared.Next();
+Console.WriteLine(randomNumber);
+
+// Using GetItems to get a random item from a collection
+var items = new[] { "apple", "banana", "cherry", "date" };
+var randomItem = Random.Shared.GetItems(items, 1).Single();
+Console.WriteLine(randomItem);
+
+// Using Shuffle to randomize a collection
+var words = "Lorem ipsum dolor sit amet".Split(' ');
+Random.Shared.Shuffle(words);
+Console.WriteLine(string.Join(' ', words));
