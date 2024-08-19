5c51bb980b8619631ad7de3c76ae3c53ddc14b9f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-08-19 17:54:44 -0400

    Add Generic Attributes

---
+++ 2024-Q3/CSharp/GenericAttributes/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+public class ConsumerBeforeCSharp11
+{
+    [BeforeCSharp11(typeof(string))]
+    public void Method() => throw new NotImplementedException();
+}
+
+public class BeforeCSharp11Attribute : Attribute
+{
+    public BeforeCSharp11Attribute(Type t) => ParamType = t;
+    public Type ParamType { get; }
+}
+
+
+public class ConsumerSinceCSharp11
+{
+    [SinceCSharp11Generic<string>]
+    public void Method() => throw new NotImplementedException();
+}
+
+public class SinceCSharp11GenericAttribute<T> : Attribute
+{
+}
