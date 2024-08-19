d6637ef3438ca34fc90d0f159a0518b7f4295f1a
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-08-19 19:09:57 -0400

    Add UTF8StringLiterals

---
+++ 2024-Q3/CSharp/UTF8StringLiterals/Program.cs
@@ -0,0 +1,7 @@
+﻿ReadOnlySpan<byte> welcomeMessage = "Welcome to the UTF-8 world!"u8;
+
+Console.WriteLine("Message in UTF-8: ");
+foreach (var b in welcomeMessage)
+{
+    Console.WriteLine($"{(char)b} ({b:X2})");
+}
