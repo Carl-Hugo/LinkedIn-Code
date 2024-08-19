71715b0271acf4a3d09467450f524281403d3d85
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-08-19 18:27:44 -0400

    Add NewlinesInStringInterpolations

---
+++ 2024-Q3/CSharp/NewlinesInStringInterpolations/Outputs.md
@@ -0,0 +1,20 @@
+```
+---[ PRODUCT ]---
+Product { Id = 2, Sku = SKU67890, Name = Smartphone, Stock = 50, IsDiscontinued = False }
+---[ PRODUCT STATUS ]---
+Product status: Limited stock, order soon
+```
+
+```
+---[ PRODUCT ]---
+Product { Id = 4, Sku = SKU98765, Name = Smartwatch, Stock = 10, IsDiscontinued = True }
+---[ PRODUCT STATUS ]---
+Product status: Discontinued, no longer available
+```
+
+```
+---[ PRODUCT ]---
+Product { Id = 5, Sku = SKU11223, Name = Tablet, Stock = 250, IsDiscontinued = False }
+---[ PRODUCT STATUS ]---
+Product status: In stock and available
+```
---
+++ 2024-Q3/CSharp/NewlinesInStringInterpolations/Program.cs
@@ -0,0 +1,26 @@
+﻿Product[] products = [
+    new Product(1, "SKU12345", "Laptop", 100, false),
+    new Product(2, "SKU67890", "Smartphone", 50, false),
+    new Product(3, "SKU54321", "Wireless Headphones", 0, false),
+    new Product(4, "SKU98765", "Smartwatch", 10, true),
+    new Product(5, "SKU11223", "Tablet", 250, false)
+];
+
+var product = Random.Shared.GetItems(products, 1).Single();
+
+var productStatus = $"Product status: {product switch
+{
+    { Stock: > 50, IsDiscontinued: false } => "In stock and available",
+    { Stock: > 0, IsDiscontinued: false } => "Limited stock, order soon",
+    { Stock: 0, IsDiscontinued: false } => "Out of stock, restocking soon",
+    { IsDiscontinued: true } => "Discontinued, no longer available",
+    _ => "Status unknown"
+}}";
+
+Console.WriteLine("---[ PRODUCT ]---");
+Console.WriteLine(product);
+
+Console.WriteLine("---[ PRODUCT STATUS ]---");
+Console.WriteLine(productStatus);
+
+public record Product(int Id, string Sku, string Name, int Stock, bool IsDiscontinued);
