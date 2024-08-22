f26e9d48cbdfa45ead799dca6b09880bc330a0c6
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-08-22 10:35:42 -0400

    Add File local types

---
+++ 2024-Q3/CSharp/FileLocalTypes/Greeter.cs
@@ -0,0 +1,15 @@
+﻿namespace Utilities;
+
+file class GreeterHelper
+{
+    public static string FormatGreeting(string name) => $"Hello, {name}!";
+}
+
+public class Greeter
+{
+    public void Greet(string name)
+    {
+        var message = GreeterHelper.FormatGreeting(name);
+        Console.WriteLine(message);
+    }
+}
---
+++ 2024-Q3/CSharp/FileLocalTypes/Program.cs
@@ -0,0 +1,8 @@
+﻿#nullable enable
+using Utilities;
+
+Console.WriteLine("Enter your name:");
+var name = Console.ReadLine();
+var greeter = new Greeter();
+var length = name.Length;
+greeter.Greet(name);
