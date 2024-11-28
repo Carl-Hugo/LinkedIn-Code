f6fc71f218e1817ae60f7ceee4943b7b5f30c80e
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-11-27 23:31:51 -0500

    Add JSON schema exporter Create Schema

---
+++ 2024/JSON schema exporter/Create Schema/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+
+JsonSerializerOptions options = JsonSerializerOptions.Default;
+JsonNode schema = options.GetJsonSchemaAsNode(typeof(AddressChangedEvent));
+Console.WriteLine(schema.ToString());
+
+public record class AddressChangedEvent(Guid UserId, string Address);
