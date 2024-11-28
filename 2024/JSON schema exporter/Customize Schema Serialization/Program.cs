8e346c57a8484d3ba4763a5d988c44e4c3233bc9
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-11-27 23:49:32 -0500

    Add Customize Schema Serialization

---
+++ 2024/JSON schema exporter/Customize Schema Serialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Schema;
+
+JsonSerializerOptions options = new(JsonSerializerOptions.Default)
+{
+    DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+};
+JsonNode schema = options.GetJsonSchemaAsNode(typeof(AddressChangedEvent));
+Console.WriteLine(schema.ToString());
+
+public record class AddressChangedEvent(Guid UserId, string Address);
