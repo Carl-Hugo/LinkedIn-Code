619cde0be4c804864d9665546a8dc0bcb520380b
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-12-17 23:39:26 -0500

    Update GlobalHttpHandler

--- 2024/GlobalHttpHandler/Features/GlobalHttpMessageHandling.cs
+++ 2024/GlobalHttpHandler/Features/GlobalHttpMessageHandling.cs
@@ -7,23 +7,11 @@ public static class WebApplicationBuilderExtensions
     public static WebApplicationBuilder AddGlobalHttpMessageHandling(this WebApplicationBuilder builder)
     {
         builder.Services
-            .AddTransient<IHttpMessageHandlerBuilderFilter, GlobalHttpMessageHandlerBuilderFilter>()
-            .AddTransient<GlobalLoggingHandler>()
+            .AddSingleton<IHttpMessageHandlerBuilderFilter, GlobalHttpMessageHandlerBuilderFilter>()
+            .AddTransient<RequestLoggingHandler>()
+            .AddTransient<ResponseTimeLoggingHandler>()
         ;
         return builder;
-    }
-}
-
-public class GlobalLoggingHandler(ILogger<GlobalLoggingHandler> logger) : DelegatingHandler
-{
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        logger.LogTrace("Request Starting: {Method} {RequestUri}", request.Method, request.RequestUri);
-        var sw = Stopwatch.StartNew();
-        var response = await base.SendAsync(request, cancellationToken);
-        sw.Stop();
-        logger.LogTrace("Response received in {responseTime} milliseconds with StatusCode {StatusCode}.", sw.ElapsedMilliseconds, response.StatusCode);
-        return response;
     }
 }
 
@@ -33,8 +21,42 @@ public class GlobalHttpMessageHandlerBuilderFilter(IServiceProvider serviceProvi
     {
         return builder =>
         {
-            builder.AdditionalHandlers.Add(serviceProvider.GetRequiredService<GlobalLoggingHandler>());
+            builder.AdditionalHandlers.Add(serviceProvider.GetRequiredService<RequestLoggingHandler>());
+            builder.AdditionalHandlers.Add(serviceProvider.GetRequiredService<ResponseTimeLoggingHandler>());
             next(builder);
         };
+    }
+}
+
+public class RequestLoggingHandler(ILogger<RequestLoggingHandler> logger) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+    CancellationToken cancellationToken)
+    {
+        logger.LogTrace(
+            "Sending request '{Method} {RequestUri}'",
+            request.Method,
+            request.RequestUri
+        );
+        var response = await base.SendAsync(request, cancellationToken);
+        return response;
+    }
+}
+
+public class ResponseTimeLoggingHandler(ILogger<ResponseTimeLoggingHandler> logger) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+    CancellationToken cancellationToken)
+    {
+        var sw = Stopwatch.StartNew();
+        var response = await base.SendAsync(request, cancellationToken);
+        sw.Stop();
+        logger.LogTrace(
+            "The request '{Method} {RequestUri}' took in {responseTime} milliseconds to respond.",
+            request.Method,
+            request.RequestUri,
+            sw.ElapsedMilliseconds
+        );
+        return response;
     }
 }
---
+++ 2024/GlobalHttpHandler/Output.md
@@ -0,0 +1,22 @@
+```bash
+info: Microsoft.Hosting.Lifetime[14]
+      Now listening on: https://localhost:7171
+info: Microsoft.Hosting.Lifetime[14]
+      Now listening on: http://localhost:5027
+info: Microsoft.Hosting.Lifetime[0]
+      Application started. Press Ctrl+C to shut down.
+info: Microsoft.Hosting.Lifetime[0]
+      Hosting environment: Development
+info: Microsoft.Hosting.Lifetime[0]
+      Content root path: e:\Repos\LinkedIn-Code\2024\GlobalHttpHandler
+trce: GlobalHttpHandler.Features.GlobalHttpMessageHandler.RequestLoggingHandler[0]
+      Sending request 'GET https://jsonplaceholder.typicode.com/users/2'
+trce: GlobalHttpHandler.Features.GlobalHttpMessageHandler.ResponseTimeLoggingHandler[0]
+      The request 'GET https://jsonplaceholder.typicode.com/users/2' took in 117 milliseconds to respond.
+trce: GlobalHttpHandler.Features.GlobalHttpMessageHandler.RequestLoggingHandler[0]
+      Sending request 'GET https://api.thedogapi.com/v1/images/ryqWuE_1T'
+trce: GlobalHttpHandler.Features.Dogs.DogApiKeyHandler[0]
+      Adding the 'x-api-key' HTTP header.
+trce: GlobalHttpHandler.Features.GlobalHttpMessageHandler.ResponseTimeLoggingHandler[0]
+      The request 'GET https://api.thedogapi.com/v1/images/ryqWuE_1T' took in 368 milliseconds to respond.
+```
