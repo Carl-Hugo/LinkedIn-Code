1d116693de8a0f2f78dfec7a1431939783d1290d
Carl-Hugo Marcotte <carlhugo@gmail.com>
2024-12-22 23:53:42 -0500

    WithExpression

---
+++ 2024/WithExpression/Program.cs
@@ -0,0 +1,15 @@
+﻿var originalOrder = new Order(1, "Laptop", 2);
+var updatedOrder = originalOrder with { Quantity = 3 };
+
+Console.WriteLine($"Original: {originalOrder}");
+Console.WriteLine($"Updated: {updatedOrder}");
+Console.WriteLine($"Same Object: {ReferenceEquals(originalOrder, updatedOrder)}");
+
+public record class Order(int Id, string Product, int Quantity);
+
+/*
+Output:
+Original: Order { Id = 1, Product = Laptop, Quantity = 2 }
+Updated: Order { Id = 1, Product = Laptop, Quantity = 3 }
+Same Object: False
+*/
