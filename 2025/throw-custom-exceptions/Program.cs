0149cdf68051900626686217307139c672d057d0
Carl-Hugo Marcotte <carlhugo@gmail.com>
2025-02-03 21:41:05 -0500

    Add throw-custom-exceptions

---
+++ 2025/throw-custom-exceptions/Program.cs
@@ -0,0 +1,28 @@
+﻿var withdrawalAmount = 100;
+var account = new { Balance = 50 };
+
+if (withdrawalAmount > account.Balance)
+{
+    throw new InsufficientFundsException(account.Balance, withdrawalAmount);
+}
+
+public class InsufficientFundsException : Exception
+{
+    public decimal Balance { get; }
+    public decimal WithdrawalAmount { get; }
+
+    public InsufficientFundsException(decimal balance, decimal withdrawalAmount)
+        : base($"Attempted to withdraw {withdrawalAmount:C}, but only {balance:C} available.")
+    {
+        Balance = balance;
+        WithdrawalAmount = withdrawalAmount;
+    }
+}
+
+
+public class InsufficientFundsExceptionAlt(decimal balance, decimal withdrawalAmount)
+     : Exception($"Attempted to withdraw {withdrawalAmount:C}, but only {balance:C} available.")
+{
+    public decimal Balance { get; } = balance;
+    public decimal WithdrawalAmount { get; } = withdrawalAmount;
+}
---
+++ 2025/throw-custom-exceptions/message.md
@@ -0,0 +1,4 @@
+```
+Unhandled exception. InsufficientFundsException: Attempted to withdraw $100.00, but only $50.00 available.
+   at Program.<Main>$(String[] args) in [...]\LinkedIn-Code\2025\throw-custom-exceptions\Program.cs:line 6
+```
